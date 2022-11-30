27a7b8644a2c9e8e2f023fe3e233680f2755b207
juan_diegohd20 <18-10143@usb.ve>
2022-11-30 12:17:49 -0400

    Se removieron las comillas de la migracion change_work_number

--- database/migrations/2022_11_30_103152_change_work_number.php
+++ database/migrations/2022_11_30_103152_change_work_number.php
@@ -15,10 +15,10 @@ class ChangeWorkNumber extends Migration
     public function up()
     {
         //
-        DB::statement("ALTER TABLE 'cotizacion_estructuras' CHANGE 'Numero_Obra' 'Numero_Obra' VARCHAR(255) NOT NULL");
-        DB::statement("ALTER TABLE 'cotizacion_estructuras' CHANGE 'Valor_Antes_Iva' 'Valor_Antes_Iva' DOUBLE(8,2) NULL; ");
-        DB::statement("ALTER TABLE 'cotizacion_estructuras' CHANGE 'AreaM2' 'AreaM2' INT(11) NULL; ");
-        DB::statement("ALTER TABLE 'cotizacion_estructuras' CHANGE 'm2' 'm2' INT(11) NULL; ");
+        DB::statement("ALTER TABLE cotizacion_estructuras CHANGE Numero_Obra Numero_Obra VARCHAR(255) NOT NULL");
+        DB::statement("ALTER TABLE cotizacion_estructuras CHANGE Valor_Antes_Iva Valor_Antes_Iva DOUBLE(8,2) NULL; ");
+        DB::statement("ALTER TABLE cotizacion_estructuras CHANGE AreaM2 AreaM2 INT(11) NULL; ");
+        DB::statement("ALTER TABLE cotizacion_estructuras CHANGE m2 m2 INT(11) NULL; ");
     }
     
     /**
@@ -29,8 +29,8 @@ class ChangeWorkNumber extends Migration
     public function down()
     {
         //
-        DB::statement("ALTER TABLE 'cotizacion_estructuras' CHANGE 'Numero_Obra' 'Numero_Obra' int(11) NOT NULL");
-        DB::statement("ALTER TABLE 'cotizacion_estructuras' CHANGE 'Valor_Antes_Iva' 'Valor_Antes_Iva' DOUBLE(8,2) NOT NULL; ");
-        DB::statement("ALTER TABLE 'cotizacion_estructuras' CHANGE 'm2' 'm2' INT(11) NOT NULL; ");
+        DB::statement("ALTER TABLE cotizacion_estructuras CHANGE Numero_Obra Numero_Obra int(11) NOT NULL");
+        DB::statement("ALTER TABLE cotizacion_estructuras CHANGE Valor_Antes_Iva Valor_Antes_Iva DOUBLE(8,2) NOT NULL; ");
+        DB::statement("ALTER TABLE cotizacion_estructuras CHANGE m2 m2 INT(11) NOT NULL; ");
     }
 }
